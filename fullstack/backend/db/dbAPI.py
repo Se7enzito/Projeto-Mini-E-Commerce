75cb73fad48cedcdb10ff3b6c60c3042285dc5cb
Se7enzito <bernardocastro2006@gmail.com>
2024-07-29 22:18:23 -0300

    Atualização no projeto

---
+++ fullstack/backend/db/dbAPI.py
@@ -0,0 +1,75 @@
+import sqlite3 as sql
+
+class Gerenciamento():
+    def __init__(self) -> None:
+        self.database = "fullstack/backend/db/database.db"
+        self.connection = None
+        self.cursor = None
+
+    def conectar(self) -> None:
+        self.connection = sql.connect(self.database)
+        self.cursor = self.connection.cursor()
+
+    def desconectar(self) -> None:
+        self.connection.close()
+        
+    def criar_tabelas(self) -> None:
+        self.conectar()
+        
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS compradores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            sobrenome TEXT NOT NULL,
+            dinheiro REAL NOT NULL,
+            cpf TEXT UNIQUE NOT NULL,
+            carrinho TEXT
+        )''')
+        
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS vendedores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            sobrenome TEXT NOT NULL,
+            cnpj TEXT UNIQUE NOT NULL
+            )''')
+        
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS itens (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            preco REAL NOT NULL,
+            vendedor_nome INTEGER,
+            FOREIGN KEY(vendedor_nome) REFERENCES vendedores(nome)
+            )''')
+        
+        self.connection.commit()
+        
+        self.desconectar()
+        
+    def criarComprador(self, nome: str, sobrenome: str, dinheiro: float, cpf: str) -> None:
+        self.conectar()
+        
+        self.cursor.execute("INSERT INTO compradores (nome, sobrenome, dinheiro, cpf) VALUES (?,?,?,?)", (nome, sobrenome, dinheiro, cpf))
+        
+        self.connection.commit()
+        
+        self.desconectar()
+        
+    def criarVendedor(self, nome: str, sobrenome: str, cnpj: str) -> None:
+        self.conectar()
+        
+        self.cursor.execute("INSERT INTO vendedores (nome, sobrenome, cnpj) VALUES (?,?,?)", (nome, sobrenome, cnpj))
+        
+        self.connection.commit()
+        
+        self.desconectar()
+        
+    def criarItem(self, nome: str, preco: float, vendedor: str) -> None:
+        self.conectar()
+        
+        self.cursor.execute("INSERT INTO itens (nome, preco, vendedor_nome) VALUES (?,?,?)", (nome, preco, vendedor))
+        
+        self.connection.commit()
+        
+        self.desconectar()
+        
+if __name__ == '__main__':
+    pass
---
+++ fullstack/backend/main.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, url_for, redirect
+from db.dbAPI import Gerenciamento
+
+app = Flask(__name__, template_folder="../frontend/templates")
+
+app.static_folder = '../frontend/src'
+app.static_url_path = '/static'
+
+geren = Gerenciamento()
+
+@app.route('/')
+def index():
+    message = ''
+    return render_template('index.html', message = message, compradores = None, vendedores = None, itens = None)
+
+@app.route('/criar_comprador', methods=['POST'])
+def criar_comprador():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        sobrenome = request.form.get('sobrenome')
+        dinheiro = request.form.get('dinheiro')
+        cpf = request.form.get('cpf')
+        
+        try:
+            dinheiro = float(dinheiro)
+        except:
+            return redirect(url_for('index', message = 'Digite apenas números no campo de Dinheiro'))
+        
+    geren.criarComprador(nome, sobrenome, dinheiro, cpf)
+    
+    return redirect(url_for('index', message = 'Comprador criado com sucesso'))
+
+@app.route('/criar_vendedor', methods=['POST'])
+def criar_vendedor():
+    if request.method == 'POST':
+        pass
+
+@app.route('/criar_item', methods=['POST'])
+def criar_item():
+    if request.method == 'POST':
+        pass
+
+@app.route('/adicionar_item', methods=['POST'])
+def adicionar_item():
+    if request.method == 'POST':
+        pass
+
+@app.route('/remover_item', methods=['POST'])
+def remover_item():
+    if request.method == 'POST':
+        pass
+
+if __name__ == '__main__':
+    geren.criar_tabelas()
+    
+    app.run(host='127.0.0.1', port=8000, debug=True)
